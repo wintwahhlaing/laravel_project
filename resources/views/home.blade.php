7df40f1fae8f77c74fb4388e074093e22a3d7947
wintwahhlaing <wintwahhlaing52016@gmail.com>
2020-07-27 00:46:57 -0700

    2nd commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,10 @@
 @extends('layout')
-
-@section('title')
-	Home page
-@endsection
-
 @section('content')
-	@foreach($data as $value)
-		{{ $value}}
-	@endforeach
+   <h2>Home Page</h2>
+   @foreach($data as $value)
+   		<li> Name- {{ $value->name }}</li>
+   		<li> Ingredients- {{ $value->ingredients }}</li>
+   		<li> Category- {{ $value->category }}</li>
+   		<hr>
+   @endforeach
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>@yield("title","Programming class")</title>
+        <!-- <title>@yield("title","Programming class")</title> -->
+        <title>Receipes Blog</title>
     </head>
     <body>
-       <ul>
+<!--        <ul>
            <a href="php"><li>PHP page</li></a>
            <a href="js"><li>JS page</li></a>
-       </ul> 
+       </ul>  -->
        @yield("content")
     </body>
 </html>
